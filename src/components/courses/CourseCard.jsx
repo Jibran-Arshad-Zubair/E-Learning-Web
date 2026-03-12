a5a8258711401cd42c4126e32f2d37de67a340a7
Jibran Arshad <ranajibranarshad@gmail.com>
2026-03-12 14:07:21 +0500

    Fixes

--- src/components/courses/CourseCard.jsx
+++ src/components/courses/CourseCard.jsx
@@ -63,11 +63,11 @@ function CourseCard({ course, isPriority = false }) {
               <Badge label="New" color="green" />
             </div>
           )}
-          {discount > 0 && (
+          {/* {discount > 0 && (
             <div className="absolute top-3 right-3 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded">
               -{discount}%
             </div>
-          )}
+          )} */}
         </div>
 
         {/* Content */}
